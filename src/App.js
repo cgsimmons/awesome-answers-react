3f3fa0d17f4e35a051471c007093f02ba41529d9
cgsimmons <chris.simmons.alt@gmail.com>
2016-11-25 13:53:01 -0800

    Use json to get questions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
+const BASE_URL = 'http://localhost:5000'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      questions: []
+    }
+  }
+
+//retrieve questions and store in state
+  getQuestion(){
+    $.ajax({
+      url: `${BASE_URL}/api/v1/questions`,
+      success: function(questions){
+        this.setState({questions: questions})
+        //bind because setting state
+      }.bind(this)
+    })
+  }
+
+  componentDidMount(){
+    this.getQuestion();
+  }
   render() {
     return (
       <div className="App">
-      
+        {
+          JSON.stringify(this.state.questions)
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import $ from 'jquery';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// TODO remove on deploy (allows variable access globally in console)
+window.$ = $;
